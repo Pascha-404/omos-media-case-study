577a14939d5ace0cbeac5b1cf530c4e66a369216
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2024-07-19 17:45:46 +0000

    feat: create TextInjector component
    Created TextInjector component and filled it with logic to output text content recieved from the 'text' property.

---
+++ src/components/TextInjector.vue
@@ -0,0 +1,11 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    text: { type: String, required: true }
+  }
+})
+</script>
+
+<template>{{ text }}</template>
