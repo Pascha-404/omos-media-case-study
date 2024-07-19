3efb9133a57dacfa46dd904d5fa073e9c325e111
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2024-07-19 21:37:03 +0000

    style: formate code after prettier config change

--- src/components/TextInjector.vue
+++ src/components/TextInjector.vue
@@ -1,15 +1,15 @@
 <script lang="ts">
-import { defineComponent, type PropType } from 'vue'
+import { defineComponent, type PropType } from 'vue';
 
 export default defineComponent({
-  props: {
-    text: {
-      type: String as PropType<string>,
-      required: true,
-      default: 'Provide a text property'
-    }
-  }
-})
+    props: {
+        text: {
+            type: String as PropType<string>,
+            required: true,
+            default: 'Provide a text property',
+        },
+    },
+});
 </script>
 
 <template>{{ text }}</template>
--- src/components/__tests__/TextInjector.spec.ts
+++ src/components/__tests__/TextInjector.spec.ts
@@ -1,16 +1,16 @@
-import { mount } from '@vue/test-utils'
-import TextInjector from '../TextInjector.vue'
+import { mount } from '@vue/test-utils';
+import TextInjector from '../TextInjector.vue';
 
 describe('TextInjector.vue', () => {
-  it('Renders provided text', () => {
-    const wrapper = mount(TextInjector, { props: { text: 'Test Text' } })
+    it('Renders provided text', () => {
+        const wrapper = mount(TextInjector, { props: { text: 'Test Text' } });
 
-    expect(wrapper.text()).toBe('Test Text')
-  })
+        expect(wrapper.text()).toBe('Test Text');
+    });
 
-  it('Renders default text', () => {
-    const wrapper = mount(TextInjector)
+    it('Renders default text', () => {
+        const wrapper = mount(TextInjector);
 
-    expect(wrapper.text()).toBe('Provide a text property')
-  })
-})
+        expect(wrapper.text()).toBe('Provide a text property');
+    });
+});
