005be845896204f280a0035083e44501b9463ade
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2024-07-20 17:38:47 +0000

    test: add tests for new functions of component
    - Added tests to verify functionality of adding new attributes
    
    - Added tests to check updates to existing attributes

--- src/components/__tests__/TextInjector.spec.ts
+++ src/components/__tests__/TextInjector.spec.ts
@@ -18,6 +18,39 @@ describe('TextInjector.vue', () => {
         expect(renderedElement.exists()).toBe(true);
         expect(renderedElement.text()).toBe('Test Text');
         expect(renderedElement.attributes('class')).toBe('insert-sick-styling-here');
-        expect(renderedElement.attributes('data-test')).toBe('test-me')
+        expect(renderedElement.attributes('data-test')).toBe('test-me');
+    });
+
+    it("Updates and add's attributes", () => {
+        // Create a mock element
+        const mockElement = document.createElement('button');
+        mockElement.setAttribute('data-soldout', 'Sold Out');
+        mockElement.setAttribute('data-adding', 'Adding...');
+        mockElement.setAttribute('data-added', 'Thank you!');
+        mockElement.setAttribute('type', 'button');
+        mockElement.setAttribute('name', 'add');
+        mockElement.innerHTML = 'Add to Cart';
+
+        // Mount the component with the mock element and changed attributes
+        const wrapper = mount(TextInjector, {
+            props: {
+                text: 'In den Warenkorb',
+                el: mockElement,
+                newAttributes: {
+                    'data-soldout': 'Ausverkauft',
+                    'data-adding': 'Füge hinzu...',
+                    'data-added': 'Dankeschön!',
+                },
+            },
+        });
+
+        const renderedElement = wrapper.find('button');
+        expect(renderedElement.exists()).toBe(true);
+        expect(renderedElement.text()).toBe('In den Warenkorb');
+        expect(renderedElement.attributes('data-soldout')).toBe('Ausverkauft');
+        expect(renderedElement.attributes('data-adding')).toBe('Füge hinzu...');
+        expect(renderedElement.attributes('data-added')).toBe('Dankeschön!');
+        expect(renderedElement.attributes('type')).toBe('button');
+        expect(renderedElement.attributes('name')).toBe('add');
     });
 });
