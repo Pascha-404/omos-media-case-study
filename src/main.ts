61e874e154a4f7f0f8fabb64323ce09b47dc1673
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2024-07-20 21:14:39 +0000

    feat: create function to handle text injections
    - Cleaned out file
    
    - Added necessary imports
    
    - Implemented function that handles text injection

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,47 @@
-import './assets/main.css'
+import { createApp } from 'vue';
+import TextInjector from './components/TextInjector.vue';
+import DateTimeInjector from './components/DateTimeInjector.vue';
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import {
+    textInjectionContent,
+    buttonInjectionContent,
+    dateTimeInjectionContent,
+} from './contentToInject';
+import type { ITextInjection } from './types';
 
-createApp(App).mount('#app')
+// Function to handle text injections
+const handleTextInjections = (injections: ITextInjection[]) => {
+    // Get all unique selector attributes
+    const selectorAttributes = [
+        ...new Set(injections.map((item: ITextInjection) => item.selector.attribute)),
+    ];
+
+    // Iterate over each inque selector attribute
+    selectorAttributes.forEach((attribute) => {
+        // Find elements that have this attribute
+        const domElements = document.querySelectorAll(`[${attribute}]`);
+
+        domElements.forEach((el) => {
+            // Find the corresponding injection for this element
+            const matchingInjection = injections.find(
+                (injection) =>
+                    el.getAttribute(attribute) === injection.selector.value &&
+                    el.innerHTML.trim() === injection.content.original.trim()
+            );
+
+            if (matchingInjection) {
+                // Create a new Vie instance and mount the TextInjector component
+                const app = createApp(TextInjector, {
+                    text: matchingInjection.content.translation,
+                    el: el,
+                });
+
+                const newEl = document.createElement('div');
+                el.replaceWith(newEl);
+                app.mount(newEl);
+            }
+        });
+    });
+};
+
+handleTextInjections(textInjectionContent);
