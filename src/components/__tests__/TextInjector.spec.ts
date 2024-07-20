878e29abc2f2bffc05dbd4b2ed7fa71c3f8ea3d6
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2024-07-20 15:39:40 +0000

    test: modify tests to work with updated TextInjector structure
    - Added tests to verify that elements are copied with correct attributes, tags, and text

--- src/components/__tests__/TextInjector.spec.ts
+++ src/components/__tests__/TextInjector.spec.ts
@@ -2,15 +2,22 @@ import { mount } from '@vue/test-utils';
 import TextInjector from '../TextInjector.vue';
 
 describe('TextInjector.vue', () => {
-    it('Renders provided text', () => {
-        const wrapper = mount(TextInjector, { props: { text: 'Test Text' } });
+    it('Creates a new element with the same attributes and updated text', () => {
+        // Create a mock element
+        const mockElement = document.createElement('h3');
+        mockElement.setAttribute('class', 'insert-sick-styling-here');
+        mockElement.setAttribute('data-test', 'test-me');
+        mockElement.innerHTML = "I'm a heading!";
 
-        expect(wrapper.text()).toBe('Test Text');
-    });
+        // Mount the component with the mock element and translated text
+        const wrapper = mount(TextInjector, {
+            props: { text: 'Test Text', el: mockElement },
+        });
 
-    it('Renders default text', () => {
-        const wrapper = mount(TextInjector);
-
-        expect(wrapper.text()).toBe('Provide a text property');
+        const renderedElement = wrapper.find('h3');
+        expect(renderedElement.exists()).toBe(true);
+        expect(renderedElement.text()).toBe('Test Text');
+        expect(renderedElement.attributes('class')).toBe('insert-sick-styling-here');
+        expect(renderedElement.attributes('data-test')).toBe('test-me')
     });
 });
