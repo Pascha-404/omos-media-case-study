894809655eea68178cfbfc98013b1bbf69a2d849
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2024-07-20 18:28:19 +0000

    refactor: move interface for attributes to types file and import
    - Moved interface to main types file for better readability and less cluttered code

--- src/components/TextInjector.vue
+++ src/components/TextInjector.vue
@@ -1,10 +1,7 @@
 <script lang="ts">
 import { defineComponent, type PropType } from 'vue';
+import type { IAttributes } from '@/types';
 import attributesToObject from '@/utils/attributesToObject';
-
-interface IAttributes {
-    [key: string]: string;
-}
 
 export default defineComponent({
     props: {
